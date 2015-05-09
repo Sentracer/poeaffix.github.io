12893f454a928c2449ee703f0867f01020379228
poeaffix <poeaffix@gmail.com>
2015-05-09 05:47:14 -0400

    Initial commit

---
+++ js/mod.js
@@ -0,0 +1,531 @@
+// JavaScript Document
+
+
+$( "#vaganflatphys" ).load( "templates/prefix.html #vaganflatphys" 
+  );
+
+$( "#1hphys" ).load( "templates/prefix.html #1hphys" 
+  );
+
+$( "#1hhybrid" ).load( "templates/prefix.html #1hhybrid" 
+  ); 
+  
+$( "#gemlvl" ).load( "templates/prefix.html #gemlvl" 
+  );
+  
+$( "#meleegem" ).load( "templates/prefix.html #meleegem" 
+  ); 
+  
+$( "#vaganlleech" ).load( "templates/prefix.html #vaganlleech" 
+  );
+  
+$( "#vaganmleech" ).load( "templates/prefix.html #vaganmleech" 
+  );  
+  
+$( "#vaganwed" ).load( "templates/prefix.html #vaganwed" 
+  );
+  
+$( "#vaganflatcold" ).load( "templates/prefix.html #vaganflatcold" 
+  );
+  
+$( "#vaganflatfire" ).load( "templates/prefix.html #vaganflatfire" 
+  );
+  
+$( "#vaganflatlight" ).load( "templates/prefix.html #vaganflatlight" 
+  ); 
+  
+$( "#catamana" ).load( "templates/prefix.html #catamana" 
+  ); 
+  
+$( "#coldgem" ).load( "templates/prefix.html #coldgem" 
+  );
+  
+$( "#firegem" ).load( "templates/prefix.html #firegem" 
+  );
+  
+$( "#lightgem" ).load( "templates/prefix.html #lightgem" 
+  );
+  
+$( "#spelldmg" ).load( "templates/prefix.html #spelldmg" 
+  );
+  
+$( "#spellhybrid" ).load( "templates/prefix.html #spellhybrid" 
+  );
+  
+$( "#toraflatphys" ).load( "templates/prefix.html #toraflatphys" 
+  );              
+  
+$( "#torahphys" ).load( "templates/prefix.html #torahphys" 
+  );
+  
+$( "#lleechn" ).load( "templates/prefix.html #lleechn" 
+  );
+  
+$( "#mleechn" ).load( "templates/prefix.html #mleechn" 
+  ); 
+  
+$( "#torawed" ).load( "templates/prefix.html #torawed" 
+  ); 
+  
+$( "#toraflatcold" ).load( "templates/prefix.html #toraflatcold" 
+  );  
+     
+$( "#toraflatfire" ).load( "templates/prefix.html #toraflatfire" 
+  );
+  
+$( "#toraflatlight" ).load( "templates/prefix.html #toraflatlight" 
+  );
+  
+$( "#2hflatphys" ).load( "templates/prefix.html #2hflatphys" 
+  ); 
+  
+$( "#2hflatcold" ).load( "templates/prefix.html #2hflatcold" 
+  );   
+  
+$( "#2hflatfire" ).load( "templates/prefix.html #2hflatfire" 
+  );  
+  
+$( "#2hflatlight" ).load( "templates/prefix.html #2hflatlight" 
+  );  
+  
+$( "#bowgem" ).load( "templates/prefix.html #bowgem" 
+  );
+  
+$( "#2hspelldmg" ).load( "templates/prefix.html #2hspelldmg" 
+  );
+  
+$( "#reflect" ).load( "templates/prefix.html #reflect" 
+  ); 
+  
+$( "#hakuarchestflat" ).load( "templates/prefix.html #hakuarchestflat" 
+  );
+  
+$( "#arar" ).load( "templates/prefix.html #arar" 
+  );          
+ 
+$( "#arhybrid" ).load( "templates/prefix.html #arhybrid" 
+  );          
+ 
+$( "#hakuchestlife" ).load( "templates/prefix.html #hakuchestlife" 
+  ); 
+ 
+$( "#hakuevchestflat" ).load( "templates/prefix.html #hakuevchestflat" 
+  ); 
+ 
+$( "#evev" ).load( "templates/prefix.html #evev" 
+  ); 
+ 
+$( "#evhybrid" ).load( "templates/prefix.html #evhybrid" 
+  );
+  
+$( "#hakuchestflates" ).load( "templates/prefix.html #hakuchestflates" 
+  );  
+  
+$( "#hakues" ).load( "templates/prefix.html #hakues" 
+  );  
+   
+$( "#eshybrid" ).load( "templates/prefix.html #eshybrid" 
+  );  
+       
+$( "#arev" ).load( "templates/prefix.html #arev" 
+  );  
+     
+$( "#arevhybrid" ).load( "templates/prefix.html #arevhybrid" 
+  ); 
+ 
+$( "#ares" ).load( "templates/prefix.html #ares" 
+  );
+  
+$( "#areshybrid" ).load( "templates/prefix.html #areshybrid" 
+  );   
+ 
+$( "#eves" ).load( "templates/prefix.html #eves" 
+  );
+  
+$( "#eveshybrid" ).load( "templates/prefix.html #eveshybrid" 
+  );
+  
+$( "#hakuarhelmflat" ).load( "templates/prefix.html #hakuarhelmflat" 
+  );  
+  
+$( "#hakuarhelmflat" ).load( "templates/prefix.html #hakuarhelmflat" 
+  );
+  
+$( "#hakuhelmlife" ).load( "templates/prefix.html #hakuhelmlife" 
+  );
+  
+$( "#miniongem" ).load( "templates/prefix.html #miniongem" 
+  ); 
+ 
+$( "#helmrarityp" ).load( "templates/prefix.html #helmrarityp" 
+  );
+  
+$( "#hakuevhelmflat" ).load( "templates/prefix.html #hakuevhelmflat" 
+  );
+  
+$( "#hakuhelmflates" ).load( "templates/prefix.html #hakuhelmflates" 
+  );    
+   
+$( "#hakuargloveflat" ).load( "templates/prefix.html #hakuargloveflat" 
+  );
+  
+$( "#hakuglovelife" ).load( "templates/prefix.html #hakuglovelife" 
+  ); 
+  
+$( "#gloverarityp" ).load( "templates/prefix.html #gloverarityp" 
+  );
+  
+$( "#gloveflatphys" ).load( "templates/prefix.html #gloveflatphys" 
+  ); 
+ 
+$( "#gloveflatcold" ).load( "templates/prefix.html #gloveflatcold" 
+  ); 
+ 
+$( "#gloveflatfire" ).load( "templates/prefix.html #gloveflatfire" 
+  ); 
+ 
+$( "#gloveflatlight" ).load( "templates/prefix.html #gloveflatlight" 
+  );  
+ 
+$( "#hakuevgloveflat" ).load( "templates/prefix.html #hakuevgloveflat" 
+  ); 
+ 
+$( "#hakugloveflates" ).load( "templates/prefix.html #hakugloveflates" 
+  );  
+ 
+$( "#movespeed" ).load( "templates/prefix.html #movespeed" 
+  );
+ 
+$( "#hakushieldflates" ).load( "templates/prefix.html #hakushieldflates" 
+  ); 
+ 
+$( "#nspelldmg" ).load( "templates/prefix.html #nspelldmg" 
+  ); 
+  
+$( "#nmana" ).load( "templates/prefix.html #nmana" 
+  );  
+  
+$( "#elreonflatphys" ).load( "templates/prefix.html #elreonflatphys" 
+  );  
+  
+$( "#elreonflatcold" ).load( "templates/prefix.html #elreonflatcold" 
+  ); 
+
+$( "#elreonflatfire" ).load( "templates/prefix.html #elreonflatfire" 
+  ); 
+
+$( "#elreonflatlight" ).load( "templates/prefix.html #elreonflatlight" 
+  );
+
+$( "#elreonar" ).load( "templates/prefix.html #elreonar" 
+  );
+
+$( "#elreones" ).load( "templates/prefix.html #elreones" 
+  );
+
+$( "#elreonev" ).load( "templates/prefix.html #elreonev" 
+  );
+
+$( "#flateseleron" ).load( "templates/prefix.html #flateseleron" 
+  );
+
+$( "#elreonlife" ).load( "templates/prefix.html #elreonlife" 
+  );
+
+$( "#mleecha" ).load( "templates/prefix.html #mleecha" 
+  );
+
+$( "#spelldmga" ).load( "templates/prefix.html #spelldmga" 
+  );
+
+$( "#weda" ).load( "templates/prefix.html #weda" 
+  );
+
+$( "#elreonevflat" ).load( "templates/prefix.html #elreonevflat" 
+  );
+
+$( "#arflatb" ).load( "templates/prefix.html #arflatb" 
+  );
+
+$( "#torablife" ).load( "templates/prefix.html #torablife" 
+  );
+
+$( "#toralifrec" ).load( "templates/prefix.html #toralifrec" 
+  );
+
+$( "#toramanrec" ).load( "templates/prefix.html #toramanrec" 
+  );
+
+$( "#quivflatcold" ).load( "templates/prefix.html #quivflatcold" 
+  );
+
+$( "#quivflatfire" ).load( "templates/prefix.html #quivflatfire" 
+  );
+  
+$( "#quivflatlight" ).load( "templates/prefix.html #quivflatlight" 
+  );  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+$( "#wepacc" ).load( "templates/suffix.html #wepacc" 
+  ); 
+  
+$( "#acchybrid" ).load( "templates/suffix.html #acchybrid" 
+  );
+  
+$( "#atkspd" ).load( "templates/suffix.html #atkspd" 
+  );                   
+      
+$( "#vagandex" ).load( "templates/suffix.html #vagandex" 
+  );
+  
+$( "#vaganstr" ).load( "templates/suffix.html #vaganstr" 
+  );
+  
+$( "#vagancrit" ).load( "templates/suffix.html #vagancrit" 
+  );
+  
+$( "#vaganmulti" ).load( "templates/suffix.html #vaganmulti" 
+  ); 
+  
+$( "#lgok" ).load( "templates/suffix.html #lgok" 
+  );
+  
+$( "#vaganlgoh" ).load( "templates/suffix.html #vaganlgoh" 
+  ); 
+  
+$( "#mgok" ).load( "templates/suffix.html #mgok" 
+  );
+  
+$( "#vagancoldres" ).load( "templates/suffix.html #vagancoldres" 
+  ); 
+  
+$( "#vaganfireres" ).load( "templates/suffix.html #vaganfireres" 
+  );
+  
+$( "#vaganlightres" ).load( "templates/suffix.html #vaganlightres" 
+  );
+  
+$( "#chaosres" ).load( "templates/suffix.html #chaosres" 
+  );
+  
+$( "#attreq" ).load( "templates/suffix.html #attreq" 
+  );                         	                 
+    
+$( "#stundur" ).load( "templates/suffix.html #stundur" 
+  );
+  
+$( "#stunthresh" ).load( "templates/suffix.html #stunthresh" 
+  );
+ 
+$( "#cataint" ).load( "templates/suffix.html #cataint" 
+  );
+  
+$( "#catamanaregen" ).load( "templates/suffix.html #catamanaregen" 
+  );
+  
+$( "#spellcrit" ).load( "templates/suffix.html #spellcrit" 
+  );
+  
+$( "#castspd" ).load( "templates/suffix.html #castspd" 
+  ); 
+  
+$( "#colddmg" ).load( "templates/suffix.html #colddmg" 
+  );
+  
+$( "#firedmg" ).load( "templates/suffix.html #firedmg" 
+  );	
+  
+$( "#lightdmg" ).load( "templates/suffix.html #lightdmg" 
+  ); 
+  
+$( "#nwepacc" ).load( "templates/suffix.html #nwepacc" 
+  );  
+  
+$( "#toraattkspd" ).load( "templates/suffix.html #toraattkspd" 
+  );  
+  
+$( "#toracrit" ).load( "templates/suffix.html #toracrit" 
+  );
+    
+$( "#toramulti" ).load( "templates/suffix.html #toramulti" 
+  ); 
+  
+$( "#nlgoh" ).load( "templates/suffix.html #nlgoh" 
+  );
+  
+$( "#toracoldres" ).load( "templates/suffix.html #toracoldres" 
+  ); 
+  
+$( "#torafireres" ).load( "templates/suffix.html #torafireres" 
+  );
+  
+$( "#toralightres" ).load( "templates/suffix.html #toralightres" 
+  );
+  
+$( "#nstundur" ).load( "templates/suffix.html #nstundur" 
+  );         
+  	
+$( "#projspd" ).load( "templates/suffix.html #projspd" 
+  );
+  
+$( "#ndex" ).load( "templates/suffix.html #ndex" 
+  ); 
+  
+$( "#hakustr" ).load( "templates/suffix.html #hakustr" 
+  );
+  
+$( "#liferegen" ).load( "templates/suffix.html #liferegen" 
+  );  
+  
+$( "#hakucoldres" ).load( "templates/suffix.html #hakucoldres" 
+  );  
+  
+$( "#hakufireres" ).load( "templates/suffix.html #hakufireres" 
+  );  
+  
+$( "#hakulightres" ).load( "templates/suffix.html #hakulightres" 
+  );  
+  
+$( "#stunrec" ).load( "templates/suffix.html #stunrec" 
+  );  
+  
+$( "#hakudex" ).load( "templates/suffix.html #hakudex" 
+  ); 
+  
+$( "#armouracc" ).load( "templates/suffix.html #armouracc" 
+  ); 
+  
+$( "#helmraritys" ).load( "templates/suffix.html #helmraritys" 
+  );  
+  
+$( "#quant" ).load( "templates/suffix.html #quant" 
+  );  
+  
+$( "#gloveraritys" ).load( "templates/suffix.html #gloveraritys" 
+  ); 
+   
+$( "#gloveattkspd" ).load( "templates/suffix.html #gloveattkspd" 
+  );
+  
+$( "#glovelgoh" ).load( "templates/suffix.html #glovelgoh" 
+  );  
+  
+$( "#lgok" ).load( "templates/suffix.html #lgok" 
+  );  
+  
+$( "#mgok" ).load( "templates/suffix.html #mgok" 
+  );  
+  
+$( "#shieldallres" ).load( "templates/suffix.html #shieldallres" 
+  );  
+    
+$( "#block" ).load( "templates/suffix.html #block" 
+  );   
+  
+$( "#nspellcrit" ).load( "templates/suffix.html #nspellcrit" 
+  );   
+  
+$( "#nmanaregen" ).load( "templates/suffix.html #nmanaregen" 
+  );  
+  
+$( "#nint" ).load( "templates/suffix.html #nint" 
+  );  
+  
+$( "#elreonacc" ).load( "templates/suffix.html #elreonacc" 
+  );  
+  
+$( "#elreonallatt" ).load( "templates/suffix.html #elreonallatt" 
+  );
+  
+$( "#elreondex" ).load( "templates/suffix.html #elreondex" 
+  );  
+  
+$( "#elreonint" ).load( "templates/suffix.html #elreonint" 
+  );  
+  
+$( "#elreonstr" ).load( "templates/suffix.html #elreonstr" 
+  );  
+  
+$( "#castspda" ).load( "templates/suffix.html #castspda" 
+  );   
+  
+$( "#amulti" ).load( "templates/suffix.html #amulti" 
+  );   
+  
+$( "#elreoncrit" ).load( "templates/suffix.html #elreoncrit" 
+  );  
+  
+$( "#acolddmg" ).load( "templates/suffix.html #acolddmg" 
+  );   
+  
+$( "#afiredmg" ).load( "templates/suffix.html #afiredmg" 
+  );   
+  
+$( "#alightdmg" ).load( "templates/suffix.html #alightdmg" 
+  );   
+  
+$( "#elreonlgoh" ).load( "templates/suffix.html #elreonlgoh" 
+  );  
+
+$( "#elreonmanaregen" ).load( "templates/suffix.html #elreonmanaregen" 
+  ); 
+
+$( "#elreoncoldres" ).load( "templates/suffix.html #elreoncoldres" 
+  ); 
+
+$( "#elreonfireres" ).load( "templates/suffix.html #elreonfireres" 
+  );
+
+$( "#elreonlightres" ).load( "templates/suffix.html #elreonlightres" 
+  );
+
+$( "#elreonallres" ).load( "templates/suffix.html #elreonallres" 
+  );
+
+$( "#elreonraritys" ).load( "templates/suffix.html #elreonraritys" 
+  );
+
+$( "#elreonacc" ).load( "templates/suffix.html #elreonacc" 
+  );
+
+$( "#ratkspd" ).load( "templates/suffix.html #ratkspd" 
+  );
+
+$( "#castspdr" ).load( "templates/suffix.html #castspdr" 
+  );
+
+$( "#nbstr" ).load( "templates/suffix.html #nbstr" 
+  );  
+  
+$( "#toraflskg" ).load( "templates/suffix.html #toraflskg" 
+  );   
+  
+$( "#toraflsku" ).load( "templates/suffix.html #toraflsku" 
+  );   
+    
+$( "#toraflskeffdur" ).load( "templates/suffix.html #toraflskeffdur" 
+  );  
+  
+$( "#quivattkspd" ).load( "templates/suffix.html #quivattkspd" 
+  );  
+    
+  
+  
+  
+  
+  
