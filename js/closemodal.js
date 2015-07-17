40722b5e6aafa475ff398b44cd9aa8e728918ae1
poeaffix <poeaffix@gmail.com>
2015-07-16 20:10:34 -0400

    close function
    added close outside modal

---
+++ js/closemodal.js
@@ -0,0 +1,381 @@
+// JavaScript Document
+
+function modalClose() {
+    if (location.hash == '#openModal100') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal0') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal1') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal2') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal3') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal4') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal5') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal6') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal7') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal8') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal9') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal10') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal11') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal12') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal13') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal14') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal15') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal16') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal17') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal18') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal19') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal20') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal21') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal22') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal23') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal24') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal25') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal26') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal27') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal28') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal29') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal30') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal31') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal32') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal33') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal34') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal35') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal36') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal37') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal38') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal39') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal40') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal41') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal42') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal43') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal44') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal45') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal46') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal47') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal48') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal49') {
+        location.hash = 'id';
+    }
+	if (location.hash == '#openModal50') {
+        location.hash = 'id';
+    }
+}
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+        modalClose();
+    }
+});
+
+var modal = document.querySelector('#openModal100');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal0');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal1');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal2');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal3');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal4');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal5');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal6');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal7');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal8');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal9');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal10');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal11');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal12');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal13');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal14');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal15');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal16');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal17');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal18');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal19');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal20');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal21');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal22');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal23');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal24');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal25');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal26');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal27');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal28');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal29');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal30');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal31');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal32');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal33');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal34');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal35');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal36');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal37');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal38');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal39');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal40');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal41');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal42');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal43');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal44');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal45');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal46');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal47');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal48');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal49');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+var modal = document.querySelector('#openModal50');
+modal.addEventListener('click', function(e) {
+    modalClose();
+}, false);
+
+
+modal.children[0].addEventListener('click', function(e) {
+    e.stopPropagation();
+}, false);
+
